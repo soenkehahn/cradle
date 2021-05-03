eade8f201e12b17e244593f269116da77cd86cb4
Sönke Hahn <soenkehahn@gmail.com>
2021-05-02 23:11:33 -0400

    wip

--- src/lib.rs
+++ src/lib.rs
@@ -172,7 +172,7 @@ mod tests {
             for x in vec![b"\x80", b"\xbf"] {
                 println!("{:?}: {:?}", x, String::from_utf8(x.to_vec()));
             }
-            cmd!("echo -e \\x80");
+            cmd!("/bin/echo -e \\x80");
         }
     }
 
