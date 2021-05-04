3aaf72df4229f8fa970bff413ff3b8948db2d543
Sönke Hahn <soenkehahn@gmail.com>
2021-05-04 01:46:07 -0400

    Add documentation

--- src/lib.rs
+++ src/lib.rs
@@ -64,6 +64,8 @@
 //! assert_eq!(output, "foo\n");
 //! ```
 //!
+//! See the implementations for [`CmdOutput`] for all the supported types.
+//!
 //! # Error Handling
 //!
 //! By default [`cmd!`] panics for a few reasons, e.g.:
@@ -144,24 +146,32 @@ pub trait CmdOutput: Sized {
     fn from_cmd_output(output: Result<Output>) -> Result<Self>;
 }
 
+/// Use this when you don't need any result from the child process.
 impl CmdOutput for () {
+    #[doc(hidden)]
     fn from_cmd_output(output: Result<Output>) -> Result<Self> {
         output?;
         Ok(())
     }
 }
 
+/// Returns what the child process writes to `stdout`.
 impl CmdOutput for String {
+    #[doc(hidden)]
     fn from_cmd_output(output: Result<Output>) -> Result<Self> {
         let output = output?;
         String::from_utf8(output.stdout).map_err(|_| Error::InvalidUtf8ToStdout)
     }
 }
 
+/// To turn all possible panics of [`cmd!`] into [`std::result::Result::Err`]s
+/// you can use a return type of `Result<T, Error>`. `T` can be any type that
+/// implements [`CmdOutput`] and [`Error`] is stir's custom error type.
 impl<T> CmdOutput for Result<T>
 where
     T: CmdOutput,
 {
+    #[doc(hidden)]
     fn from_cmd_output(output: Result<Output>) -> Result<Self> {
         Ok(match output {
             Ok(_) => T::from_cmd_output(output),
