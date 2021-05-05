e8e2b59dd78d2e3b488bff45e41710638dd312b7
Sönke Hahn <soenkehahn@gmail.com>
2021-05-05 14:19:24 -0400

    fix windows tests

--- src/context_integration_tests.rs
+++ src/context_integration_tests.rs
@@ -1,26 +1,34 @@
-use executable_path::executable_path;
-use gag::BufferRedirect;
-use std::io::{self, Read};
-use stir::cmd;
-
-fn with_gag<F>(mk_buf: fn() -> io::Result<BufferRedirect>, f: F) -> String
-where
-    F: FnOnce(),
-{
-    let mut buf = mk_buf().unwrap();
-    f();
-    let mut output = String::new();
-    buf.read_to_string(&mut output).unwrap();
-    output
+fn main() {
+    if cfg!(not(target_os = "windows")) {
+        non_windows_tests();
+    }
 }
 
-fn main() {
+#[cfg(not(target_os = "windows"))]
+fn non_windows_tests() {
+    use executable_path::executable_path;
+    use gag::BufferRedirect;
+    use std::io::{self, Read};
+    use stir::cmd;
+
+    fn with_gag<F>(mk_buf: fn() -> io::Result<BufferRedirect>, f: F) -> String
+    where
+        F: FnOnce(),
+    {
+        let mut buf = mk_buf().unwrap();
+        f();
+        let mut output = String::new();
+        buf.read_to_string(&mut output).unwrap();
+        output
+    }
+
     {
         assert_eq!(
             with_gag(BufferRedirect::stdout, || cmd!("echo foo")),
             "foo\n"
         );
     }
+
     {
         assert_eq!(
             with_gag(BufferRedirect::stderr, || cmd!(
