f3154fd5ed42a76d04d832d2429cdf317d1254d1
Sönke Hahn <soenkehahn@gmail.com>
2021-06-26 16:24:18 -0400

    Remove changelog and add MSRV to the readme (#70)

--- CHANGELOG.md
+++
@@ -1,5 +0,0 @@
-# 0.0.5
-
-- Add `Stdin`
-- Bump MSRV from `1.40` to `1.41`
-- Start a changelog
--- README.php
+++ README.php
@@ -7,3 +7,6 @@ Here's an example:
 
 For comprehensive documentation, head over to
 [docs.rs/cradle](https://docs.rs/cradle/latest/cradle/).
+
+## MSRV
+The minimal supported rust version is `0.41`.
