e332e48f469fdb10521bfca858f32653b745a5df
Sönke Hahn <soenkehahn@gmail.com>
2021-07-02 12:00:07 -0400

    Add a badge to the readme that links to crates.io (#84)

--- README.php
+++ README.php
@@ -1,4 +1,5 @@
 ![ci status badge](https://github.com/soenkehahn/cradle/actions/workflows/ci.yaml/badge.svg)
+[![crates.io](https://img.shields.io/crates/v/cradle.svg)](https://crates.io/crates/cradle)
 
 `cradle` is a library for executing commands in child processes.
 Here's an example:
