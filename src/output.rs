7582900259a994ef0138348ca162a35cd9ac194e
Sönke Hahn <soenkehahn@gmail.com>
2021-07-10 15:32:45 -0400

    Tweak Output documentation

--- src/output.rs
+++ src/output.rs
@@ -1,23 +1,54 @@
 use crate::{Config, Error, RunResult};
 use std::{process::ExitStatus, sync::Arc};
 
-/// All possible return types of [`cmd!`] have to implement this trait.
-/// For documentation about what these return types do, see the
-/// individual implementations below.
-///
-/// Except for tuples: All [`Output`] implementations for tuples serve
-/// the same purpose: combining multiple types that implement [`Output`]
-/// to retrieve more information from a child process. The following code
-/// for example retrieves what's written to `stdout` **and** the
-/// [`ExitStatus`]:
+/// All possible return types of [`cmd!`], [`cmd_unit!`] or
+/// [`cmd_result!`] must implement this trait.
+/// This return-type polymorphism makes cradle very flexible.
+/// For example if you want to capture what a command writes
+/// to its `stdout` you can do that using [`StdoutUntrimmed`]:
 ///
 /// ```
 /// use cradle::*;
 ///
-/// let (StdoutUntrimmed(stdout), Exit(status)) = cmd!(%"echo foo");
-/// assert_eq!(stdout, "foo\n");
-/// assert!(status.success());
+/// let StdoutUntrimmed(output) = cmd!(%"echo foo");
+/// assert_eq!(output, "foo\n");
 /// ```
+///
+/// But if instead you want to capture the commands exit code,
+/// you can use [`Exit`]:
+///
+/// ```
+/// use cradle::*;
+///
+/// let Exit(status) = cmd!("false");
+/// assert_eq!(status.code(), Some(1));
+/// ```
+///
+/// For documentation on what all these possible return types do,
+/// see the documentation for the individual impls of [`Output`].
+/// Here's a non-exhaustive list of the more commonly used return-types to get you started:
+///
+/// - `()`: In case you don't want to capture anything. See also [`cmd_unit`].
+/// - [`StdoutUntrimmed`], [`StdoutTrimmed`] and [`Stderr`]: To capture `stdout` and `stderr`.
+/// - [`Exit`]: To capture the commands exit code.
+///
+/// Alse, [`Output`] is implemented for a number of tuples.
+/// You can use this to combine other return-types that implement [`Output`].
+/// The following code for example retrieves the [`ExitStatus`]
+/// **and** what's written to `stdout`:
+///
+/// ```
+/// use cradle::*;
+///
+/// let (Exit(status), StdoutUntrimmed(stdout)) = cmd!(%"echo foo");
+/// assert!(status.success());
+/// assert_eq!(stdout, "foo\n");
+/// ```
+///
+/// [`StdoutUntrimmed`]: trait.Output.html#impl-Output-3
+/// [`StdoutTrimmed`]: trait.Output.html#impl-Output-2
+/// [`Stderr`]: trait.Output.html#impl-Output-1
+/// [`Exit`]: trait.Output.html#impl-Output
 pub trait Output: Sized {
     #[doc(hidden)]
     fn configure(config: &mut Config);
@@ -63,10 +94,12 @@ pub struct StdoutTrimmed(pub String);
 /// # }
 /// ```
 impl Output for StdoutTrimmed {
+    #[doc(hidden)]
     fn configure(config: &mut Config) {
         StdoutUntrimmed::configure(config);
     }
 
+    #[doc(hidden)]
     fn from_run_result(config: &Config, result: Result<RunResult, Error>) -> Result<Self, Error> {
         let StdoutUntrimmed(stdout) = StdoutUntrimmed::from_run_result(config, result)?;
         Ok(StdoutTrimmed(stdout.trim().to_owned()))
