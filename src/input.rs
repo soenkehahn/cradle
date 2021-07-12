bd73d9b7c8108615a40011ad2c614eebff8558e1
Sönke Hahn <soenkehahn@gmail.com>
2021-07-11 23:14:52 -0400

    Update src/input.rs
    Co-authored-by: Casey Rodarmor <casey@rodarmor.com>

--- src/input.rs
+++ src/input.rs
@@ -80,7 +80,8 @@ use std::{
 /// // Works for slices:
 /// let hex = to_hex(Stdin(&[14, 15, 16]));
 /// assert_eq!(hex, "0E0F10");
-/// // Also works for vectors:
+///
+/// // Vectors:
 /// let hex = to_hex(Stdin(vec![14, 15, 16]));
 /// assert_eq!(hex, "0E0F10");
 /// // Also works for multiple arguments using tuples:
