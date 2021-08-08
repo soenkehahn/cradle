8e5e2891c3c0696b419c0f0be42898f47a0f367b
Sönke Hahn <soenkehahn@gmail.com>
2021-08-08 11:45:06 -0400

    KiB
    Co-authored-by: Casey Rodarmor <casey@rodarmor.com>

--- memory-test/src/bin/produce_bytes.rs
+++ memory-test/src/bin/produce_bytes.rs
@@ -4,7 +4,7 @@ use std::io::{stdout, Write};
 fn main() -> Result<()> {
     let mut args = std::env::args();
     let mut bytes: usize = args.nth(1).unwrap().parse()?;
-    eprintln!("producing {} kB", bytes / 2_usize.pow(10));
+    eprintln!("producing {} KiB", bytes / 2_usize.pow(10));
     let buffer = &[b'x'; 1024];
     let mut stdout = stdout();
     while bytes > 0 {
