789536ebdeb8dc3bddf68c3920fe4686dd370826
Sönke Hahn <soenkehahn@gmail.com>
2021-08-08 11:45:57 -0400

    KiB
    Co-authored-by: Casey Rodarmor <casey@rodarmor.com>

--- memory-test/src/bin/cradle_user.rs
+++ memory-test/src/bin/cradle_user.rs
@@ -3,6 +3,6 @@ use cradle::prelude::*;
 fn main() {
     let mut args = std::env::args();
     let bytes: usize = args.nth(1).unwrap().parse().unwrap();
-    eprintln!("consuming {} kB", bytes / 2_usize.pow(10));
+    eprintln!("consuming {} KiB", bytes / 2_usize.pow(10));
     cmd_unit!("./target/release/produce_bytes", bytes.to_string());
 }
