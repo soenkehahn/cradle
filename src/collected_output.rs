4015e1cf32d8d09fad82facf9e82ed97a3328a76
Sönke Hahn <soenkehahn@gmail.com>
2021-08-09 22:40:42 -0400

    Dry up output stream handling

--- src/collected_output.rs
+++ src/collected_output.rs
@@ -13,12 +13,40 @@ pub(crate) struct Waiter {
 }
 
 impl Waiter {
+    fn spawn_standard_stream_handler(
+        capture_stream: bool,
+        mut source: impl Read + Send + 'static,
+        mut relay_sink: impl Write + Send + 'static,
+    ) -> JoinHandle<io::Result<Option<Vec<u8>>>> {
+        thread::spawn(move || -> io::Result<Option<Vec<u8>>> {
+            let mut collected = if capture_stream {
+                Some(Vec::new())
+            } else {
+                None
+            };
+            let buffer = &mut [0; 256];
+            loop {
+                let length = source.read(buffer)?;
+                if (length) == 0 {
+                    break;
+                }
+                if let Some(collected_stdout) = &mut collected {
+                    collected_stdout.extend(&buffer[..length]);
+                }
+                if !capture_stream {
+                    relay_sink.write_all(&buffer[..length])?;
+                }
+            }
+            Ok(collected)
+        })
+    }
+
     pub(crate) fn spawn_standard_stream_relaying<Stdout, Stderr>(
         context: &Context<Stdout, Stderr>,
         config: &Config,
         mut child_stdin: ChildStdin,
-        mut child_stdout: ChildStdout,
-        mut child_stderr: ChildStderr,
+        child_stdout: ChildStdout,
+        child_stderr: ChildStderr,
     ) -> Self
     where
         Stdout: Write + Send + Clone + 'static,
@@ -29,52 +57,16 @@ impl Waiter {
             child_stdin.write_all(&config_stdin)?;
             Ok(())
         });
-        let mut context_clone = context.clone();
-        let capture_stdout = config.capture_stdout;
-        let stdout_join_handle = thread::spawn(move || -> io::Result<Option<Vec<u8>>> {
-            let mut collected_stdout = if capture_stdout {
-                Some(Vec::new())
-            } else {
-                None
-            };
-            let buffer = &mut [0; 256];
-            loop {
-                let length = child_stdout.read(buffer)?;
-                if (length) == 0 {
-                    break;
-                }
-                if let Some(collected_stdout) = &mut collected_stdout {
-                    collected_stdout.extend(&buffer[..length]);
-                }
-                if !capture_stdout {
-                    context_clone.stdout.write_all(&buffer[..length])?;
-                }
-            }
-            Ok(collected_stdout)
-        });
-        let mut context_clone = context.clone();
-        let capture_stderr = config.capture_stderr;
-        let stderr_join_handle = thread::spawn(move || -> io::Result<Option<Vec<u8>>> {
-            let mut collected_stderr = if capture_stderr {
-                Some(Vec::new())
-            } else {
-                None
-            };
-            let buffer = &mut [0; 256];
-            loop {
-                let length = child_stderr.read(buffer)?;
-                if (length) == 0 {
-                    break;
-                }
-                if let Some(collected_stderr) = &mut collected_stderr {
-                    collected_stderr.extend(&buffer[..length]);
-                }
-                if !capture_stderr {
-                    context_clone.stderr.write_all(&buffer[..length])?;
-                }
-            }
-            Ok(collected_stderr)
-        });
+        let stdout_join_handle = Self::spawn_standard_stream_handler(
+            config.capture_stdout,
+            child_stdout,
+            context.stdout.clone(),
+        );
+        let stderr_join_handle = Self::spawn_standard_stream_handler(
+            config.capture_stderr,
+            child_stderr,
+            context.stderr.clone(),
+        );
         Waiter {
             stdin: stdin_join_handle,
             stdout: stdout_join_handle,
