5006c2de8e5f937b1a58024a25403bfd15dc46d2
Sönke Hahn <soenkehahn@gmail.com>
2021-09-10 17:00:11 -0400

    wip

--- README.php
+++ README.php
@@ -2,7 +2,9 @@
 [![crates.io](https://img.shields.io/crates/v/cradle.svg)](https://crates.io/crates/cradle)
 [![docs](https://docs.rs/cradle/badge.svg)](https://docs.rs/cradle)
 
-`cradle` is a library for executing commands in child processes.
+`cradle` is a library for executing child processes.
+It provides a more convenient interface than
+[std::process::Command](https://doc.rust-lang.org/std/process/struct.Command.html).
 Here's an example:
 
 ``` rust
@@ -14,3 +16,7 @@ For comprehensive documentation, head over to
 
 ## MSRV
 The minimal supported rust version is `0.41`.
+
+## Design Goals
+
+`cradle` is meant to bla
